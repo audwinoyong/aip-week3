419f0192acfcd64b80d356f229277a360cae9791
Audwin Oyong <11814771@student.uts.edu.au>
2018-08-09 18:00:49 +1000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aip-week3';
+  number = 3;
+
+  substract() {
+    this.number--;
+  }
 }
